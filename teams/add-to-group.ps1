e121e4f28b132c3e77d13f181606e265f965ed02
fady soliman <72667975+fadysaad66@users.noreply.github.com>
2025-07-12 23:14:30 +0400

    Add files via upload

---
+++ teams/add-to-group.ps1
@@ -0,0 +1,81 @@
+
+Function InstallModules
+{
+Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
+Install-Module -Name ImportExcel -Force
+}
+
+
+function ImportModules
+{
+Import-Module ExchangeOnlineManagement
+Import-Module ImportExcel
+
+}
+
+function ConnectToExchange
+{
+Connect-ExchangeOnline 
+
+}
+
+function AddEmailsToGroup {
+    param (
+        [string]$groupId,           # The email address or name of the Microsoft 365 Group (Unified Group)
+        [string]$excelFilePath,     # Path to the Excel file
+        [string]$sheetName = "Sheet1"  # Name of the worksheet in the Excel file
+    )
+
+    # Import data from Excel
+    try {
+        $emails = Import-Excel -Path $excelFilePath -WorksheetName $sheetName
+        Write-Host "Imported data from Excel:" -ForegroundColor Green
+        $emails | Format-Table -AutoSize
+    } catch {
+        Write-Host "Failed to import Excel data. Error: $_" -ForegroundColor Red
+        return
+    }
+
+    # Loop to add emails to the microsoft 365 outlook group
+    foreach ($entry in $emails) {
+        # column name in excel named Email
+        $userEmail = $entry.Email   
+        if (-not [string]::IsNullOrWhiteSpace($userEmail)) {
+            Write-Host "Attempting to add email: $userEmail to the Microsoft 365 Group" -ForegroundColor Yellow
+
+            try {
+                Add-UnifiedGroupLinks -Identity $groupId -LinkType Members -Links $userEmail -ErrorAction Stop
+                Write-Host "Successfully added $userEmail to $groupId." -ForegroundColor Green
+            } catch {
+                Write-Host "An error occurred while trying to add $userEmail. Error: $_" -ForegroundColor Red
+            }
+        }
+    }
+}
+
+
+
+
+    #install modules
+      # InstallModules
+
+    #import modules
+       ImportModules
+
+    #connect to teams
+      ConnectToExchange
+
+# Main script execution
+
+# Prompt user to enter the team ID and Excel file path
+
+$groupId = Read-Host -Prompt "Please enter the Group ID"
+
+$excelFilePath = Read-Host -Prompt "Please enter the path to the Excel file"
+
+  # Call the function to add users to the team
+AddEmailsToGroup -groupId $groupId -excelFilePath $excelFilePath
+
+
+
+
